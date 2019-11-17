8af03b333e98f99e302d3c50869a335b0e1bbf35
EC2 Default User <ec2-user@ip-172-31-39-1.us-east-2.compute.internal>
2019-11-17 16:57:15 +0000

    tema2

--- main/app.js
+++ main/app.js
@@ -1,5 +1,31 @@
 function addTokens(input, tokens){
+    if(typeof input!='string'){
+        throw new Error('Invalid input');
+    }
+    if(input.length < 6)
+    {
+        throw new Error('Input should have at least 6 characters');
+    }
     
+   for(var i in tokens)
+   {
+       if(typeof tokens[i].tokenName!=='string')
+       {
+           throw new Error('Invalid array format');
+       }
+       else
+       {
+           if(input.includes('...'))
+           {
+                input=input.replace('...','${'+tokens[i].tokenName+'}');
+                return input;
+           }
+           else
+           {
+               return input;
+           }
+       }
+   }
 }
 
 const app = {
